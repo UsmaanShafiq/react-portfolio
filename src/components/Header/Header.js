5564b63fa0948c36a3c221b761eaa9e863a0b969
UsmaanShafiq <usmanshafiq933@gmail.com>
2024-01-13 23:15:26 +0500

    removed old code

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,54 +8,7 @@ import styles from './Header.module.css'
 
 function Header() {
   return (
-
-    <Box component="header" 
-      sx={{
-        p: 5, 
-        display:'flex', 
-        justifyContent:'space-between', 
-        alignItems:'center'
-      }} 
-      className='header_wrapper'>
-      
-      <Box component="div" className="row"
-      sx={{
-        display:'flex', 
-        justifyContent:'space-between', 
-        alignItems:'center',
-        width:'100%'
-      }} 
-      >
-        <div className={styles.logo_col}>
-          <NavLink to="/">
-            <Logo/>
-          </NavLink>
-        </div>
-        
-        <div className={styles.nav_col}>
-          <Navbar/>
-        </div>
-            
-
-        <div className={styles.btn_col}>
-            <Button variant="contained" 
-                sx={{
-                  p:'20px 40px', 
-                  fontFamily:'DM Sans', 
-                  fontSize:'16px', 
-                  fontWeight:'700', 
-                  backgroundColor:'#5956E9',
-                  borderRadius:'12px'}}>
-                  Let's Talk
-            </Button>
-        </div>
-      </Box>
-      
-      
-      
-
-    </Box>
-
+    
   )
 }
 
